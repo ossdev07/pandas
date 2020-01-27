f59eb05d121bab480f108d3eaa1639fdb053a8c3
ossdev07 <ossdev@puresoftware.com>
2020-01-27 12:29:15 +0000

    rebasing files

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-IS_SUDO=""
-ARCHICONDA_PYTHON="python3.7"
 # edit the locale file if needed
 if [[ "$(uname)" == "Linux" && -n "$LC_ALL" ]]; then
     echo "Adding locale to the first line of pandas/__init__.py"
@@ -14,13 +12,8 @@ if [[ "$(uname)" == "Linux" && -n "$LC_ALL" ]]; then
     echo
 fi
 
+MINICONDA_DIR="$HOME/miniconda3"
 
-if [ `uname -m` = 'aarch64' ]; then
-   MINICONDA_DIR="$HOME/archiconda3"
-   IS_SUDO="sudo"
-else
-   MINICONDA_DIR="$HOME/miniconda3"
-fi
 
 if [ -d "$MINICONDA_DIR" ]; then
     echo
@@ -43,24 +36,9 @@ else
   exit 1
 fi
 
-if [ `uname -m` = 'aarch64' ]; then
-   wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O archiconda.sh
-   chmod +x archiconda.sh
-   $IS_SUDO apt-get install python-dev
-   $IS_SUDO apt-get install python3-pip
-   $IS_SUDO apt-get install lib$ARCHICONDA_PYTHON-dev
-   $IS_SUDO apt-get install xvfb
-   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/local/lib:/usr/local/bin/python
-   ./archiconda.sh -b
-   echo "chmod MINICONDA_DIR"
-   $IS_SUDO chmod -R 777 $MINICONDA_DIR
-   $IS_SUDO cp $MINICONDA_DIR/bin/* /usr/bin/
-   $IS_SUDO rm /usr/bin/lsb_release
-else
-   wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$CONDA_OS.sh" -O miniconda.sh
-   chmod +x miniconda.sh
-   ./miniconda.sh -b
-fi
+wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$CONDA_OS.sh" -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
 
 export PATH=$MINICONDA_DIR/bin:$PATH
 
@@ -72,8 +50,8 @@ echo
 echo "update conda"
 conda config --set ssl_verify false
 conda config --set quiet true --set always_yes true --set changeps1 false
-$IS_SUDO conda install pip  # create conda to create a historical artifact for pip & setuptools
-$IS_SUDO conda update -n base conda
+conda install pip  # create conda to create a historical artifact for pip & setuptools
+conda update -n base conda
 
 echo "conda info -a"
 conda info -a
@@ -113,18 +91,8 @@ conda list
 conda remove --all -q -y -n pandas-dev
 
 echo
-if [ `uname -m` = 'aarch64' ]; then
-    $IS_SUDO chmod -R 777 $MINICONDA_DIR
-    $IS_SUDO conda install botocore
-    $IS_SUDO conda install numpy
-    $IS_SUDO conda install python-dateutil=2.8.0
-    $IS_SUDO conda install hypothesis
-    $IS_SUDO conda install pytz
-    $IS_SUDO chmod -R 777 $MINICONDA_DIR
-fi
-
 echo "conda env create -q --file=${ENV_FILE}"
-time $IS_SUDO conda env create -q --file="${ENV_FILE}"
+time conda env create -q --file="${ENV_FILE}"
 
 
 if [[ "$BITS32" == "yes" ]]; then
@@ -138,17 +106,18 @@ source activate pandas-dev
 echo
 echo "remove any installed pandas package"
 echo "w/o removing anything else"
-$IS_SUDO conda remove pandas -y --force || true
-if [ `uname -m` = 'aarch64' ]; then
-    $IS_SUDO $ARCHICONDA_PYTHON -m pip uninstall -y pandas || true
-else
-    pip uninstall -y pandas || true
-fi
+conda remove pandas -y --force || true
+pip uninstall -y pandas || true
 
 echo
 echo "remove postgres if has been installed with conda"
 echo "we use the one from the CI"
-$IS_SUDO conda remove postgresql -y --force || true
+conda remove postgresql -y --force || true
+
+echo
+echo "remove qt"
+echo "causes problems with the clipboard, we use xsel for that"
+conda remove qt -y --force || true
 
 echo
 echo "conda list pandas"
@@ -157,10 +126,7 @@ conda list pandas
 # Make sure any error below is reported as such
 
 echo "[Build extensions]"
-if [ `uname -m` = 'aarch64' ]; then
-    sudo chmod -R 777 /home/travis/.ccache
-fi
-python setup.py build_ext -q -i
+python setup.py build_ext -q -i -j2
 
 # XXX: Some of our environments end up with old versions of pip (10.x)
 # Adding a new enough version of pip to the requirements explodes the
@@ -169,40 +135,21 @@ python setup.py build_ext -q -i
 # - py35_compat
 # - py36_32bit
 echo "[Updating pip]"
-if [ `uname -m` = 'aarch64' ]; then
-    sudo chmod -R 777 /home/travis/archiconda3/envs/pandas-dev/lib/$ARCHICONDA_PYTHON/site-packages
-    #$IS_SUDO $ARCHICONDA_PYTHON -m pip install pytest-forked
-    #$IS_SUDO $ARCHICONDA_PYTHON -m pip install pytest-xdist
-    $IS_SUDO $ARCHICONDA_PYTHON -m pip install --no-deps -U pip wheel setuptools
-    sudo chmod -R 777 $MINICONDA_DIR
-else
-    python -m pip install --no-deps -U pip wheel setuptools
-fi
+python -m pip install --no-deps -U pip wheel setuptools
 
 echo "[Install pandas]"
-if [ `uname -m` = 'aarch64' ]; then
-    $IS_SUDO chmod -R 777 $MINICONDA_DIR
-    $IS_SUDO $ARCHICONDA_PYTHON -m pip install numpy
-    $IS_SUDO $ARCHICONDA_PYTHON -m pip install hypothesis
-    $IS_SUDO chmod -R 777 /home/travis/.cache/
-    $IS_SUDO $ARCHICONDA_PYTHON -m pip install --no-build-isolation -e .
-else
-    python -m pip install --no-build-isolation -e .
-fi
+python -m pip install --no-build-isolation -e .
 
 echo
 echo "conda list"
 conda list
 
 # Install DB for Linux
+
 if [[ -n ${SQL:0} ]]; then
   echo "installing dbs"
-  if [ `uname -m` = 'aarch64' ]; then
-    sudo systemctl start mysql
-  else
-    psql -c 'create database pandas_nosetest;' -U postgres
-  fi
   mysql -e 'create database pandas_nosetest;'
+  psql -c 'create database pandas_nosetest;' -U postgres
 else
    echo "not using dbs on non-linux Travis builds or Azure Pipelines"
 fi
